03e9f1893b9b0311a1b97299a821e3a988425ad8
haaseeli <37594075+haaseeli@users.noreply.github.com>
2018-03-25 14:07:13 +0200

    Add files via upload

---
+++ Aufgabe00/00.js
@@ -0,0 +1,7 @@
+function Eingabefeld() {
+    var eingabe = prompt("Name", "");
+    if (eingabe != null) {
+        document.getElementById("Type").innerHTML =
+            "Merci goku, " + eingabe + ".";
+    }
+}
