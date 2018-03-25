d9a272e2928dc6c1a9cb1da1b1d917b7bf62d9ef
haaseeli <37594075+haaseeli@users.noreply.github.com>
2018-03-25 14:06:53 +0200

    Delete aufgabe00.ts

--- aufgabe00.ts
+++
@@ -1,7 +0,0 @@
-function Eingabefeld() {
-  var eingabe = prompt("Name", "");
-  if (eingabe != null) {
-    document.getElementById("Type").innerHTML =
-      "Merci goku, " + eingabe + ".";
-  }
-}
