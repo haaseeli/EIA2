e0e936834417526d005e5f2df3480fac6a3a58b0
Elisabeth Ha <37594075+haaseeli@users.noreply.github.com>
2018-04-14 00:08:23 +0200

    Add files via upload

---
+++ Aufgabe02/memory.js
@@ -0,0 +1,7 @@
+var memory;
+(function (memory) {
+    var numPlayers;
+    var numPairs;
+    var cardContent = ["M", "O", "I", "R", "A"];
+})(memory || (memory = {}));
+//# sourceMappingURL=memory.js.map
---
+++ Aufgabe02/memory.ts
@@ -0,0 +1,8 @@
+namespace memory {
+    
+    let numPlayers: number;
+    let numPairs: number;
+    let cardContent: string[] = ["M", "O", "I", "R", "A"];
+    
+    }
+
