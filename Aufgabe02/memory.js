f58448b9fa80d6ed409ad5d0cfc10f84b166cde6
ElisabethHaase <ElisabethHaase@DESKTOP-RLCD4C7>
2018-04-15 16:25:00 +0200

    Update

--- Aufgabe02/memory.js
+++ Aufgabe02/memory.js
@@ -9,10 +9,14 @@ Datum: 21.06.17
  */
 var MemoryGame;
 (function (MemoryGame) {
+    document.addEventListener("DOMContentLoaded", init); //Abfragen-Funktion f�r Spielerzahl und Paarzahl
     let spielerzahl;
     let paarzahl;
     let cardContent = [1, 2, 28, 10, 1995, 42, 1776, 37];
     ;
     let karten = [];
+    //Abfragen-Funktion Spielerzahl und Paarzahl prompts:
+    spielerzahl = parseInt(prompt('Spielerzahl:'));
+    paarzahl = parseInt(prompt('Mit wie vielen Paaren wird gespielt? (5 bis 8 verf�gbar)'));
 })(MemoryGame || (MemoryGame = {}));
 //# sourceMappingURL=memory.js.map
--- Aufgabe02/memory.ts
+++ Aufgabe02/memory.ts
@@ -10,9 +10,14 @@
 
 namespace MemoryGame {
     
+    //Abfragen-Funktion für Spielerzahl und Paarzahl
+    document.addEventListener("DOMContentLoaded", init); 
+    
+    
     let spielerzahl: number;
     let paarzahl: number;
     let cardContent: number[] = [1, 2, 28, 10, 1995, 42, 1776, 37];
+    
     
     interface karte {
         cardContent: number;
@@ -20,7 +25,37 @@ namespace MemoryGame {
     };
     
     
+    
     let karten: karte[] = []; 
     
     
-    }
+    //Abfragen-Funktion Spielerzahl und Paarzahl prompts:
+    spielerzahl = parseInt(prompt('Spielerzahl:'));
+    paarzahl = parseInt(prompt('Mit wie vielen Paaren wird gespielt? (5 bis 8 verfügbar)'));
+    
+    
+    //header mit Spielerinfo
+    function createHeader(): void {
+        
+        let info: HTMLElement = document.createElement("header");
+            document.body.appendChild(info);
+        
+        let h2: HTMLHeadingElement = document.createElement("h2");
+            h2.innerHTML = "Spieler";
+            info.appendChild(h2);
+        
+            for (let i: number = 0; i < _player; i++) {
+                let paragraph: HTMLpElement = document.createElement("p");
+                info.appendChild(paragraph);
+                
+        let boxLegend: HTMLLegendElement = document.createElement("legend");
+                boxLegend.innerHTML = "Spieler " + (i + 1) + ":";
+                box.appendChild(boxLegend); 
+                
+                let boxContent: HTMLParagraphElement = document.createElement("p");
+                boxContent.innerHTML = "Score: 0";
+                box.appendChild(boxContent);
+        }
+        
+    
+}
