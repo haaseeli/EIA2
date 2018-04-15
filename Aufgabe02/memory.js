ac5bbbb29bef8be137824b594d5980923dc29a75
ElisabethHaase <ElisabethHaase@DESKTOP-RLCD4C7>
2018-04-15 17:37:26 +0200

    ahhhhhhhhhhhhhhh

--- Aufgabe02/memory.js
+++ Aufgabe02/memory.js
@@ -14,16 +14,27 @@ var MemoryGame;
     let spielerzahl;
     let paarzahl;
     let cardContent = [1, 2, 28, 10, 1995, 42, 1776, 37];
+    let cardArray = [];
+    let playerInfo;
+    let cardField;
     ;
     let karten = [];
-    function init() {
+    function main() {
         //Abfragen-Funktion Spielerzahl und Paarzahl prompts:
         spielerzahl = parseInt(prompt("Spielerzahl:", ""), 4);
         spielerzahl > 4 ? spielerzahl = 4 : spielerzahl = spielerzahl;
-        paarzahl = parseInt(prompt("Mit wie vielen Paaren wird gespielt? (5 bis 8 verf�gbar)", ""), 8);
+        paarzahl = parseInt(prompt("Mit wie vielen Paaren wird gespielt? (5 bis 8 verfuegbar)", ""), 8);
         if (paarzahl < 5 || paarzahl > 8) {
             paarzahl = 8;
         }
     }
+    //Karten werden erstellt
+    function createCard(_cardContent, _status) {
+        let card = document.createElement("div");
+        card.innerText = _cardContent;
+        card.setAttribute("class", "card " + _status);
+        cardArray.push(card);
+    }
+    document.addEventListener("DOMContentLoaded", main);
 })(MemoryGame || (MemoryGame = {}));
 //# sourceMappingURL=memory.js.map
--- Aufgabe02/memory.ts
+++ Aufgabe02/memory.ts
@@ -9,42 +9,52 @@
      */
 
 namespace MemoryGame {
-    
+
     //Abfragen-Funktion für Spielerzahl und Paarzahl
-    document.addEventListener("DOMContentLoaded", init); 
-    
-    
+    document.addEventListener("DOMContentLoaded", init);
+
+
     let spielerzahl: number;
     let paarzahl: number;
     let cardContent: number[] = [1, 2, 28, 10, 1995, 42, 1776, 37];
-    
-    
+    let cardArray: HTMLElement[] = [];
+
+    let playerInfo: HTMLElement;
+    let cardField: HTMLElement;
+
+
     interface karte {
-        cardContent: number;
-        status: string; 
+        cardContent: number,
+        status: string
     };
-    
-    
-    
-    let karten: karte[] = []; 
-    
-   
-    
-    function init(): void {
-        
-        
-    //Abfragen-Funktion Spielerzahl und Paarzahl prompts:
-    spielerzahl = parseInt(prompt("Spielerzahl:", ""), 4);
-    spielerzahl > 4 ? spielerzahl = 4 : spielerzahl = spielerzahl;
-    
-    paarzahl = parseInt(prompt("Mit wie vielen Paaren wird gespielt? (5 bis 8 verfügbar)", ""), 8);
-    if (paarzahl < 5 || paarzahl > 8) {
-        paarzahl = 8;
+
+
+
+    let karten: karte[] = [];
+
+
+
+    function main(): void {
+
+
+        //Abfragen-Funktion Spielerzahl und Paarzahl prompts:
+        spielerzahl = parseInt(prompt("Spielerzahl:", ""), 4);
+        spielerzahl > 4 ? spielerzahl = 4 : spielerzahl = spielerzahl;
+
+        paarzahl = parseInt(prompt("Mit wie vielen Paaren wird gespielt? (5 bis 8 verfuegbar)", ""), 8);
+        if (paarzahl < 5 || paarzahl > 8) {
+            paarzahl = 8;
         }
     }
-    
 
-   
+    //Karten werden erstellt
+    function createCard(_cardContent: string, _status: string): void {
+        let card: HTMLElement = document.createElement("div");
+        card.innerText = _cardContent;
+        card.setAttribute("class", "card " + _status);
+        cardArray.push(card);
+    }
 
-    
+
+    document.addEventListener("DOMContentLoaded", main);
 }
