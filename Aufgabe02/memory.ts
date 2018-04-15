d09f22437d6a1486084330bc9a91f40193138903
ElisabethHaase <ElisabethHaase@DESKTOP-RLCD4C7>
2018-04-15 16:31:31 +0200

    Update: Slowly getting somewhere

--- Aufgabe02/memory.ts
+++ Aufgabe02/memory.ts
@@ -33,29 +33,12 @@ namespace MemoryGame {
     spielerzahl = parseInt(prompt('Spielerzahl:'));
     paarzahl = parseInt(prompt('Mit wie vielen Paaren wird gespielt? (5 bis 8 verfügbar)'));
     
-    
     //header mit Spielerinfo
     function createHeader(): void {
         
         let info: HTMLElement = document.createElement("header");
             document.body.appendChild(info);
         
-        let h2: HTMLHeadingElement = document.createElement("h2");
-            h2.innerHTML = "Spieler";
-            info.appendChild(h2);
-        
-            for (let i: number = 0; i < _player; i++) {
-                let paragraph: HTMLpElement = document.createElement("p");
-                info.appendChild(paragraph);
-                
-        let boxLegend: HTMLLegendElement = document.createElement("legend");
-                boxLegend.innerHTML = "Spieler " + (i + 1) + ":";
-                box.appendChild(boxLegend); 
-                
-                let boxContent: HTMLParagraphElement = document.createElement("p");
-                boxContent.innerHTML = "Score: 0";
-                box.appendChild(boxContent);
-        }
         
     
 }
