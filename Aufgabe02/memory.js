fc09e69e6cef1c5524d00f7f47e7fcecf75e3c56
ElisabethHaase <ElisabethHaase@DESKTOP-RLCD4C7>
2018-04-15 15:01:22 +0200

    Update

--- Aufgabe02/memory.js
+++ Aufgabe02/memory.js
@@ -1,7 +1,7 @@
 var memory;
 (function (memory) {
-    var numPlayers;
-    var numPairs;
-    var cardContent = ["M", "O", "I", "R", "A"];
+    let numPlayers;
+    let numPairs;
+    let cardContent = ["M", "O", "I", "R", "A"];
 })(memory || (memory = {}));
 //# sourceMappingURL=memory.js.map
--- Aufgabe02/memory.ts
+++ Aufgabe02/memory.ts
@@ -1,8 +1,29 @@
-namespace memory {
+    /*
+    Aufgabe: Aufgabe 13
+    Name: Elisabeth Haase
+    Matrikel: 256436
+    Datum: 21.06.17
+
+     Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+     Er wurde nicht kopiert und auch nicht diktiert.
+     */
+
+namespace MemoryGame {
     
-    let numPlayers: number;
-    let numPairs: number;
-    let cardContent: string[] = ["M", "O", "I", "R", "A"];
+    interface Memory {
+        
+        CardContent: number;
+        Status: string;
+        
+        }
+    
+    let eins: Memory = {
+        CardContent: 1,
+        Status: ["open", "taken", "hidden"]
+        }
+        
+       
     
     }
-
+    
+    
