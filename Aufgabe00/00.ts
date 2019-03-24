a1ceb00d34c603f11bb4764cd7b1a1e76836750a
Elisabeth Ha <37594075+haaseeli@users.noreply.github.com>
2019-03-24 01:58:14 +0100

    Update 00.ts
    uff VSC will nicht

--- Aufgabe00/00.ts
+++ Aufgabe00/00.ts
@@ -1,9 +1,10 @@
-
 // Pop Up Eingabefeld
+
     function Eingabefeld() {
-        var eingabe: string = prompt("Name", ""); // Typ Annotiation var eingabe
+        var eingabe: string = prompt("Name", ""); // var eingabe ist Typ string
         if (eingabe != null) {
             document.getElementById("Type").innerHTML =
                 "Merci goku, " + eingabe + ".";
+                console.log ( "Merci goku, " + eingabe + ".");
         } // if close
     } // function close
