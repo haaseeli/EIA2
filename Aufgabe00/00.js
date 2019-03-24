908addcedfec54cab502f849f2914b63e9e28c43
Elisabeth Ha <37594075+haaseeli@users.noreply.github.com>
2019-03-24 01:59:01 +0100

    Update 00.js
    .

--- Aufgabe00/00.js
+++ Aufgabe00/00.js
@@ -4,6 +4,7 @@ function Eingabefeld() {
     if (eingabe != null) {
         document.getElementById("Type").innerHTML =
             "Merci goku, " + eingabe + ".";
+        console.log ( "Merci goku, " + eingabe + ".");
     } // if close
 } // function close
 //# sourceMappingURL=00.js.map
