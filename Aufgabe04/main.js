9c351b41c98a49834e92632cd28302aeeda61667
Eli <eli.haase@web.de>
2019-04-21 15:29:37 +0200

    final1

--- Aufgabe04/main.js
+++ Aufgabe04/main.js
@@ -4,6 +4,7 @@ var Aufgabe04;
     window.addEventListener("load", init);
     function init(_event) {
         let fieldsets = document.getElementsByTagName("fieldset");
+        document.getElementById("button").addEventListener("click", pressedButton);
         for (let i = 0; i < fieldsets.length; i++) {
             let fieldset = fieldsets[i];
             fieldset.addEventListener("change", handler);
@@ -14,137 +15,137 @@ var Aufgabe04;
         switch (ziel.name) {
             case ("Erdbeere"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis1").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis1").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis1 = Number(ziel.value);
-                    preis(0.50, preis1, 0);
+                    preis(0, preis1, 0);
                 }
                 else {
                     document.getElementById("eis1").innerHTML = "";
                     let preis1 = 0;
-                    preis(0.50, 0, preis1);
+                    preis(1, 0, preis1);
                 }
                 break;
             }
             case ("Vanille"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis2").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis2").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis2 = Number(ziel.value);
-                    preis(0.50, preis2, 0);
+                    preis(0, preis2, 0);
                 }
                 else {
                     document.getElementById("eis2").innerHTML = "";
                     let preis2 = 0;
-                    preis(0.50, 0, preis2);
+                    preis(1, 0, preis2);
                 }
                 break;
             }
             case ("Kokos"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis3").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis3").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis3 = Number(ziel.value);
-                    preis(0.50, preis3, 0);
+                    preis(0, preis3, 0);
                 }
                 else {
                     document.getElementById("eis3").innerHTML = "";
                     let preis3 = 0;
-                    preis(0.50, 0, preis3);
+                    preis(1, 0, preis3);
                 }
                 break;
             }
             case ("Himbeere"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis4").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis4").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis4 = Number(ziel.value);
-                    preis(0.50, preis4, 0);
+                    preis(0, preis4, 0);
                 }
                 else {
                     document.getElementById("eis4").innerHTML = "";
                     let preis4 = 0;
-                    preis(0.50, 0, preis4);
+                    preis(1, 0, preis4);
                 }
                 break;
             }
             case ("Schokolade"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis5").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis5").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis5 = Number(ziel.value);
-                    preis(0.50, preis5, 0);
+                    preis(0, preis5, 0);
                 }
                 else {
                     document.getElementById("eis5").innerHTML = "";
                     let preis5 = 0;
-                    preis(0.50, 0, preis5);
+                    preis(1, 0, preis5);
                 }
                 break;
             }
             case ("Apfel"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis6").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis6").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis6 = Number(ziel.value);
-                    preis(0.50, preis6, 0);
+                    preis(0, preis6, 0);
                 }
                 else {
                     document.getElementById("eis6").innerHTML = "";
                     let preis6 = 0;
-                    preis(0.50, 0, preis6);
+                    preis(1, 0, preis6);
                 }
                 break;
             }
             case ("Kiwi"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis7").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis7").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis7 = Number(ziel.value);
-                    preis(0.50, preis7, 0);
+                    preis(0, preis7, 0);
                 }
                 else {
                     document.getElementById("eis7").innerHTML = "";
                     let preis7 = 0;
-                    preis(0.50, 0, preis7);
+                    preis(1, 0, preis7);
                 }
                 break;
             }
             case ("Melone"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis8").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis8").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis8 = Number(ziel.value);
-                    preis(0.50, preis8, 0);
+                    preis(0, preis8, 0);
                 }
                 else {
                     document.getElementById("eis8").innerHTML = "";
                     let preis8 = 0;
-                    preis(0.50, 0, preis8);
+                    preis(1, 0, preis8);
                 }
                 break;
             }
             case ("Haselnuss"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis9").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis9").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis9 = Number(ziel.value);
-                    preis(0.50, preis9, 0);
+                    preis(0, preis9, 0);
                 }
                 else {
                     document.getElementById("eis9").innerHTML = "";
                     let preis9 = 0;
-                    preis(0.50, 0, preis9);
+                    preis(1, 0, preis9);
                 }
                 break;
             }
             case ("Zitrone"): {
                 if (ziel.checked == true) {
-                    document.getElementById("eis10").innerHTML = "1x  " + ziel.name;
+                    document.getElementById("eis10").innerHTML = "1x  " + ziel.name + " " + "|";
                     let preis10 = Number(ziel.value);
-                    preis(0.50, preis10, 0);
+                    preis(0, preis10, 0);
                 }
                 else {
                     document.getElementById("eis10").innerHTML = "";
                     let preis10 = 0;
-                    preis(0.50, 0, preis10);
+                    preis(1, 0, preis10);
                 }
                 break;
             }
             case ("Waffel oder Becher"): {
                 if (ziel.checked == true) {
-                    document.getElementById("woderb").innerHTML = ziel.id;
+                    document.getElementById("woderb").innerHTML = ziel.id + "," + "";
                 }
                 else {
                     document.getElementById("woderb").innerHTML = "";
@@ -153,85 +154,95 @@ var Aufgabe04;
             }
             case ("Portion Sahne"): {
                 if (ziel.checked == true) {
-                    document.getElementById("topping1").innerHTML = ziel.id;
+                    document.getElementById("topping1").innerHTML = ziel.id + "," + "";
                     let preistopping = Number(ziel.value);
-                    preis(0.30, preistopping, 0);
+                    preis(0.50, preistopping, 0);
                 }
                 else {
                     document.getElementById("topping1").innerHTML = "";
                     let preistopping = 0;
-                    preis(0.30, 0, preistopping);
+                    preis(0.50, 0, preistopping);
                 }
                 break;
             }
             case ("Schokosoße"): {
                 if (ziel.checked == true) {
-                    document.getElementById("topping2").innerHTML = ziel.id;
+                    document.getElementById("topping2").innerHTML = ziel.id + "," + "";
                     let preistopping = Number(ziel.value);
-                    preis(0.40, preistopping, 0);
+                    preis(0.50, preistopping, 0);
                 }
                 else {
                     document.getElementById("topping2").innerHTML = "";
                     let preistopping = 0;
-                    preis(0.40, 0, preistopping);
+                    preis(0.50, 0, preistopping);
                 }
                 break;
             }
             case ("Erdbeersoße"): {
                 if (ziel.checked == true) {
-                    document.getElementById("topping3").innerHTML = ziel.id;
+                    document.getElementById("topping3").innerHTML = ziel.id + "," + "";
                     let preistopping = Number(ziel.value);
-                    preis(0.40, preistopping, 0);
+                    preis(0.50, preistopping, 0);
                 }
                 else {
                     document.getElementById("topping3").innerHTML = "";
                     let preistopping = 0;
-                    preis(0.40, 0, preistopping);
+                    preis(0.50, 0, preistopping);
                 }
                 break;
             }
             case ("Schokostreusel"): {
                 if (ziel.checked == true) {
-                    document.getElementById("topping4").innerHTML = ziel.id;
+                    document.getElementById("topping4").innerHTML = ziel.id + "," + "";
                     let preistopping = Number(ziel.value);
-                    preis(0.20, preistopping, 0);
+                    preis(0.50, preistopping, 0);
                 }
                 else {
                     document.getElementById("topping4").innerHTML = "";
                     let preistopping = 0;
-                    preis(0.20, 0, preistopping);
+                    preis(0.50, 0, preistopping);
                 }
                 break;
             }
             case ("Gummibärchen"): {
                 if (ziel.checked == true) {
-                    document.getElementById("topping5").innerHTML = ziel.id;
+                    document.getElementById("topping5").innerHTML = ziel.id + "," + "";
                     let preistopping = Number(ziel.value);
-                    preis(0.30, preistopping, 0);
+                    preis(0.50, preistopping, 0);
                 }
                 else {
                     document.getElementById("topping5").innerHTML = "";
                     let preistopping = 0;
-                    preis(0.30, 0, preistopping);
+                    preis(0.50, 0, preistopping);
                 }
                 break;
             }
             case ("Lieferoption"): {
                 if (ziel.checked == true) {
-                    document.getElementById("lieferoption").innerHTML = "Ihre gewählte Versandart: " + ziel.id;
+                    document.getElementById("versandart").innerHTML = "Ihre gewählte Versandart: " + ziel.id;
                 }
                 else {
-                    document.getElementById("lieferoption").innerHTML = "";
+                    document.getElementById("versandart").innerHTML = "";
                 }
                 break;
             }
         }
-        function preis(_hilfestellung, _preisaddition, _preissubstracion) {
-            gesamtpreis = gesamtpreis - _hilfestellung + _preisaddition - _preissubstracion;
-            if (gesamtpreis < 0) {
-                gesamtpreis = 0;
-            }
-            document.getElementById("gesamtpreis").innerHTML = "Der Gesamtpreis der Bestellung beträgt: " + gesamtpreis + "€";
+    }
+    function preis(_hilfestellung, _preisaddition, _preissubstracion) {
+        gesamtpreis = gesamtpreis + _preisaddition - _preissubstracion - _hilfestellung;
+        if (gesamtpreis < 0) {
+            gesamtpreis = 0;
+        }
+        document.getElementById("gesamtpreis").innerHTML = "Der Gesamtpreis der Bestellung beträgt: " + gesamtpreis + "€";
+    }
+    function pressedButton(_event) {
+        let ziel = _event.target;
+        console.log("pressed");
+        if (ziel.checked == false || gesamtpreis == 0) {
+            document.getElementById("buttonangaben").innerHTML = "Es wurden wichtige Daten noch nicht ausgefüllt";
+        }
+        else {
+            document.getElementById("buttonangaben").innerHTML = "";
         }
     }
 })(Aufgabe04 || (Aufgabe04 = {}));
